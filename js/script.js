0c73a7764b0e80fdf4324888b601111f1b886432
shahnewazshanto22 <shahnewazshanto22@gmail.com>
2023-06-12 23:16:15 +0600

    added cuda website

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready (function() {
+
+// ======= skill progress bar js ==========
+  $('.skill_bar').circlechart();
+  
+// ======= mixitUp js for portfolio section ==========
+  var mixer = mixitup ('.mixitup-main');
+})
+
+    
